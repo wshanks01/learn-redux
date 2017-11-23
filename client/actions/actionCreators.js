678a9e5356368b27daec34fab3fb9bab41bbdebc
wshanks01 <william.shanks2@gmail.com>
2017-11-23 10:08:36 -0500

    first commit

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,26 @@
+// Increment
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+// Add Comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+// Remove Comment
+export function removeComment(postId, i) {
+  return {
+    type:'REMOVE_COMMENT',
+    postId,
+    i
+  }
+}
